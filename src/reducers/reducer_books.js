04c026fdf3a54cb3f7a7bcda252b79fd0729d605
Emily Bie <bieyuyu@gmail.com>
2017-04-11 21:58:14 +1000

    add book list and book detail containers and hook up with redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: the good parts', pages: 367},
+    { title: 'Harry Potter', pages: 532},
+    { title: 'Jane Erye', pages: 312},
+    { title: 'Pride and Prejudice', pages: 421}
+  ];
+}
